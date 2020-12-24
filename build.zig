833ef3a1e5fb5b84b799f3cd4c0e3f02fb9bf7c8
gregvardy <vardygreg23@gmail.com>
2020-12-24 21:09:10 +0200

    added build file

---
+++ build.zig
@@ -0,0 +1,22 @@
+const Builder = @import("std").build.Builder;
+
+pub fn build(b: *Builder) void {
+    const exe = b.addExecutable("zig-tracer", "src/main.zig");
+    exe.setBuildMode(b.standardReleaseOptions());
+    exe.linkSystemLibrary("c");
+
+    if (exe.target.isDarwin()) {
+        exe.addIncludeDir("/usr/local/include/SDL2");
+        exe.linkFramework("SDL2");
+    } else {
+        exe.linkSystemLibrary("SDL2");
+    }
+
+    exe.install();
+
+    const run = b.step("run", "Run the project");
+    const run_cmd = exe.run();
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    run.dependOn(&run_cmd.step);
+}
